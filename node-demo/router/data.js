f5684f92d77751a51d3a8ddd3ae4758210d5e94f
byshi <byshi@ronglian.com>
2018-05-23 19:26:53 +0800

    sh

--- node-demo/router/data.js
+++ node-demo/router/data.js
@@ -1,5 +1,6 @@
 
 var qs= require('querystring');
+const http = require('http');
 const https = require('https');
 const url = require('url');
 const html = require('./html.js')
@@ -11,9 +12,13 @@ let dataRouter = {
         let method = req.method;
         if(pName=='login'&&method.toLowerCase()=='post'){
             this.login(req,pathname,res)
-        }else if(pathname == '/data/weather/city'){
+        }
+        else if(pathname == '/data/weather/city'){
             var data = qs.stringify(urlInfo.query);
             this.getCityWeather(data,res)    
+        }
+        else if(pathname == '/data/weather/citylist'){
+            this.getCityList(data,res)
         }
     },
     login(req,pathname,res){
@@ -39,14 +44,48 @@ let dataRouter = {
             method: 'GET',
             data:data
         }
+        this.requestMthods(options,res)
+    },
+    getCityList(data,res){
+        var options={
+            hostname:'api.yytianqi.com',
+            path:'/citylist/id/1',
+            method: 'GET'
+        }
         this.requestMthod(options,res)
     },
     requestMthod(options,respones){
         var alldata = ''; 
-        var city=options.data; 
+        var city=options.data;
+        if(!city){
+            city='';
+        }
+        var req = http.request({
+            host:options.hostname,
+            path:options.path + city,
+            method:options.method
+        }, function (res) { 
+            res.on('data', function (chunk) { 
+                alldata += chunk; 
+            });
+            res.on('end',()=>{
+                respones.writeHead(200,{'Content-Type':'application/json'})
+                respones.end(alldata);
+            })
+        }); 
+           
+        req.on('error', function (e) { 
+            console.log('problem with request: ' + e.message); 
+        }); 
+          
+        req.end();
+    },
+    requestMthods(options,respones){
+        var alldata = ''; 
+        var city=options.data;
         var req = https.request({
             host:options.hostname,
-            path:options.path + options.data,
+            path:options.path + city,
             method:options.method
         }, function (res) { 
             res.on('data', function (chunk) { 
