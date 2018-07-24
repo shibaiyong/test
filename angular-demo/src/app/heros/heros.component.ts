ce7445dd3633734d897dcf4da33f7a359f3242a5
byshi <byshi@ronglian.com>
2018-07-24 11:15:00 +0800

    shibaiyong edite ngFor of angular

--- angular-demo/src/app/heros/heros.component.ts
+++ angular-demo/src/app/heros/heros.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Hero} from '../heros';
+import {Hero} from '../heros';//导入的Hero;作用是规定写入数据的类型。
 import { HEROES } from '../mock-heroes';
 
 @Component({
@@ -9,11 +9,15 @@ import { HEROES } from '../mock-heroes';
 })
 
 export class HerosComponent implements OnInit {
-    //下面是该组件的属性
-    hero: Hero = {
+    //下面是该组件的属性，Hero限制数据的类型
+    // hero: Hero = {
+    //     id:1,
+    //     name:'shibaiyong'
+    // };
+    selectHero:Hero = {
         id:1,
         name:'shibaiyong'
-    };
+    };//视图层的selectHero未定义。会报错,ngIf可以解决;2.或者给一个默认值。
     heroes = HEROES;
     constructor(){
 
@@ -21,7 +25,9 @@ export class HerosComponent implements OnInit {
 
     ngOnInit(){
 
+    };
+
+    onSelect(hero:Hero):void{//void还没有研究。写不写不影响
+        this.selectHero=hero;
     }
-
-
 }
