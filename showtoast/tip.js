1944364b0bb64d3880a5db5ea5cd5abfd763c8df
shibaiyong <shibaiyong@example.com>
2019-04-01 09:48:02 +0800

    shibaiyong change

--- showtoast/tip.js
+++ showtoast/tip.js
@@ -4,19 +4,22 @@
 function ShowTip(options){
     this.elem = options.elem;
     this.content = options.content;
+    this.title = options.title;
 }
 
 ShowTip.prototype={
     inite:function(callbacks){
         $(this.elem).show();
+        $(this.elem).find('.toast_title').text(this.title);
         $(this.elem).find('.showtoastcontent').text(this.content);
         this.timeOutHide();
-        this.clearTimeOut();
     },
     timeOutHide:function(){
+        var that = this;
         var $this = $(this.elem);
         this.time = setTimeout(function(){
             $this.hide();
+            that.clearTimeOut();
         },5000)
     },
     clearTimeOut:function(){
