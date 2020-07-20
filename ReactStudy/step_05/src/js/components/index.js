e6c0fb800b10d8cc9c391b5a5da5dfb9deca780b
shibaiyong <shibaiyong@example.com>
2020-07-20 12:46:09 +0800

    ‘shibaiyong change

--- ReactStudy/step_05/src/js/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Head } from './Head'
-export { default as Nav } from './Nav'
-export { default as Content } from './Content'
--- ReactStudy/step_05/src/js/containers/index.js
+++
@@ -1,4 +0,0 @@
-export {default as App} from './App'
-
-// 首页
-export { default as Home } from './Home'
